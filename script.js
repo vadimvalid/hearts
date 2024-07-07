ee416b7cfbe44f419ed283ca5cf9ab51392dc962
Vadim Gadjimuradov <vadiminsk90@gmail.com>
2024-07-07 17:59:57 +0300

    Add mobile header

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-const tabs = document.querySelectorAll(".section .tabs");
+(function () {
+  const header = document.querySelector(".header");
+  const burgerBtn = document.querySelector(".header__burger");
 
-tabs.forEach((tab) => {
-  const tabButtons = tab.querySelectorAll('[role="tab"]');
-  const tabPanels = tab.querySelectorAll('[role="tabpanel"]');
-
-  function handleTabClick(event) {
-    // hide all tab panels
-    tabPanels.forEach((panel) => {
-      panel.hidden = true;
-    });
-    // mark all tabs as unselected
-    tabButtons.forEach((tab) => {
-      // tab.ariaSelected = false;
-      tab.setAttribute("aria-selected", false);
-    });
-    event.currentTarget.setAttribute("aria-selected", true);
-    // find the associated tabPanel and show it!
-    const { id } = event.currentTarget;
-    const tabPanel = tab.querySelector(`[aria-labelledby="${id}"]`);
-    tabPanel.hidden = false;
-  }
-
-  tabButtons.forEach((button) =>
-    button.addEventListener("click", handleTabClick)
-  );
-});
+  burgerBtn.addEventListener("click", () => {
+    header.classList.toggle("header--active");
+  });
+})();
